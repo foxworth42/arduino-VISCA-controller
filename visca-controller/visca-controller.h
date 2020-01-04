13dd24bd36dcb745491bbb4c35b05f6679eb0198
Ed Walker <github@foxwire.org>
2020-01-03 21:47:55 -0800

    Add some quick comments about what the definitions are for.

--- visca-controller/visca-controller.h
+++ visca-controller/visca-controller.h
@@ -6,6 +6,9 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
+// Pin assignments from arduno shield.
+
+// Analog inputs
 #define PAN 0
 #define TILT 1
 #define ZOOM 2
@@ -13,9 +16,11 @@
 #define AUX2 4
 #define AUX3 5
 
+// Serial I/O for VISCA
 #define VISCARX 2
 #define VISCATX 3
 
+// Button inputs
 #define BTN1 4
 #define BTN2 5
 #define BTN3 6
